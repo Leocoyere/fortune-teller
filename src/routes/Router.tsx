016d88802281a39dd136f74d793e0a967411d140
leo <leo.coyere94@gmail.com>
2025-04-16 13:00:15 +0200

    Page Not Found

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router'
 import App from '@/pages/App.tsx'
 import Answer from '@/pages/Answer.tsx'
 import CardProvider from '@/components/providers/CardProvider.tsx'
+import Error404 from '@/pages/Error404'
 
 export default function Router() {
     return (
@@ -15,6 +16,7 @@ export default function Router() {
                 </CardProvider>
                 }
             />
+            <Route path="*" element={<Error404 />} />
         </Routes>
     )
 }
